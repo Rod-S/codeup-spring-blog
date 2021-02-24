3ff556fd711f6b8c7b718d3d116f55734668356a
Rod-S <rodsanati@gmail.com>
2021-02-23 23:44:04 -0600

    add post controller and postrepository jpa

--- src/main/java/spring/springblog/controllers/PostController.java
+++ src/main/java/spring/springblog/controllers/PostController.java
@@ -1,21 +1,35 @@
 package spring.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import spring.springblog.models.Post;
+import spring.springblog.repositories.PostRepository;
 
 @Controller
 public class PostController {
 
+    private final PostRepository postsDao;
+
+    public PostController(PostRepository postsDao){
+        this.postsDao = postsDao;
+    }
+
     @GetMapping("/posts")
-    public String getPosts() {
+    public String postsIndex(Model model) {
+
+        model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String getPostId(@PathVariable int id) {
+    public String postView(Model model, @PathVariable long id) {
+//        get single post by id later
+        Post post = postsDao.getOne(id);
+        model.addAttribute("post", post);
         return "posts/show";
     }
 
