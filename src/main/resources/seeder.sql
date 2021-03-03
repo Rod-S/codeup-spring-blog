c363e2a1f11b298c5c485da6f0bde470039dc303
Rod-S <rodsanati@gmail.com>
2021-03-03 09:22:01 -0600

    changes to header

--- src/main/resources/seeder.sql
+++ src/main/resources/seeder.sql
@@ -4,8 +4,4 @@ insert into posts (title, body) values ('background', 'ac nulla sed vel enim sit
 insert into posts (title, body) values ('heuristic', 'magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae');
 insert into posts (title, body) values ('website', 'duis consequat dui nec nisi volutpat eleifend donec ut dolor morbi vel');
 insert into posts (title, body) values ('system engine', 'ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus');
-User user = new User();
-user.setUsername("cody");
-user.setEmail("cody@codeup.com");
-user.setPassword("codeuprocks");
-userDao.save(user);
+
